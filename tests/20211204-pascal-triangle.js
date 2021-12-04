99a103083604a32ec4d8bbc16773f55390f64b59
eunjinlee <lucy7xoxo@naver.com>
2021-12-04 23:37:45 +0900

    20211204

--- tests/20211204-pascal-triangle.js
+++ tests/20211204-pascal-triangle.js
@@ -8,9 +8,13 @@ var getRow = function (rowIndex) {
   row.push(1); // [1]
 
   for (let i = 1; i <= rowIndex; i++) {
-    // 이전 row의 길이보다 1 작은 숫자 만큼 for문을 돈다.
-    for (let j = row.length - 1; j > 0; j--) {}
-
-    row[rowIndex][i] = row[rowIndex - 1][i - 1] + row[rowIndex - 1][i];
+    // 이전 row의 길이보다 1 작은 숫자 만큼 for문을 돈다. 맨 마지막은 1임.
+    for (let j = row.length - 1; j > 0; j--) {
+      row[j] = row[j - 1] + row[j];
+    }
+    // 맨 마지막은 직접 넣어준다. i = 1인 경우 j for문은 타지 않음.
+    row.push(1);
   }
+
+  return row;
 };

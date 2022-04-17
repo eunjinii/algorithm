a8ad5fa70f8c71eb5c60cb667d96fdbcfa7937ed
eunjinlee <lucy7xoxo@naver.com>
2022-04-17 18:00:18 +0900

    2022-04-17 풀이 주석 수정

--- tests/20220417-topological-sort.js
+++ tests/20220417-topological-sort.js
@@ -1,7 +1,6 @@
 // 알고리즘 문제해결전략
 // dfs - 고대어 사전 문제
 
-// 5, ['gg', 'kia', 'lotte', 'lg', 'hanwha']
 const makeGraph = (numOfTest, words) => {
     let graph = Array.from(Array(26), () => Array(26).fill(0)); // 1. 2차원 배열을 26*26 크기로 초기화해줌.
 
@@ -23,20 +22,6 @@ const makeGraph = (numOfTest, words) => {
     return graph;
 };
 
-// let order = [];
-// let visited = Array(26).fill(0);
-
-// const dfs = (here) => {
-//     visited[here] = 1;
-
-//     for (let there = 0; there < graph.length; there++) {
-//         if (graph[here][there] && !visited[there]) {
-//             dfs(there);
-//         }
-//     }
-//     order.push(here);
-// };
-
 const topologicalSort = (numOfTest, words) => {
     const graph = makeGraph(numOfTest, words);
     const length = graph.length;
@@ -44,6 +29,9 @@ const topologicalSort = (numOfTest, words) => {
     let order = [];
     let visited = Array(26).fill(false);
 
+    /**
+     * 깊이 우선 탐색 dfs
+     */
     const dfs = (here) => {
         visited[here] = true;
 
@@ -56,16 +44,24 @@ const topologicalSort = (numOfTest, words) => {
         order.push(here); // 위상정렬 결과 얻기 위해 dfs() 종료 시마다 현재 정점 기록. 그 기록 순서를 뒤집으면 위상정렬 결과임
     };
 
+    /**
+     * 전체 노드에 대한 깊이 우선 탐색인 dfsAll() 역할
+     */
     for (let i = 0; i < length; i++) {
         if (!visited[i]) {
             dfs(i);
         }
     }
 
-    // console.log(order);
+    /**
+     * 위상정렬 결과 도출하기 위하여 순서 뒤집는다.
+     */
     order.reverse();
 
-    // 그래프를 2중 for문을 돌면서, 반대로(n -> m) 연결된 간선이 있으면 빈 배열 리턴한다.(반대 간선 있으면 사이클이기 때문.)
+    /**
+     * 문제 요구사항에서 위상정렬 여부를 판단해야 하므로
+     * 그래프를 2중 for문을 돌면서, 반대로(n -> m) 연결된 간선이 있으면 빈 배열 리턴한다.(반대 간선 있으면 사이클이기 때문.)
+     */
     for (let m = 0; m < length; m++) {
         for (let n = m + 1; n < length; n++) {
             if (graph[order[n]][order[m]]) {
@@ -74,7 +70,10 @@ const topologicalSort = (numOfTest, words) => {
         }
     }
 
-    let answer = undefined;
+    /**
+     * 문제 요구사항 도출 위해 order 값을 알파벳으로 변환, 또는 "INVALID HYPOTHESIS" 리턴.
+     */
+    let answer;
     if (order.length === 0) {
         answer = "INVALID HYPOTHESIS";
     } else {
@@ -89,6 +88,20 @@ const topologicalSort = (numOfTest, words) => {
     return answer;
 };
 
-// makeGraph(3, ["ba", "aa", "ab"]);
-// topologicalSort(5, ["gg", "kia", "lotte", "lg", "hanwha"]);
+topologicalSort(5, ["gg", "kia", "lotte", "lg", "hanwha"]);
+// zyxwvutsrqponmjigklhfedcba - 위상위상정렬 특성상 여러 답이 존재할 수 있는데, 선후관계가 위반하지 않으면 남은 알파벳은 어떤 순서든 상관 없다.
+// o-g-k-l-h 순서만 맞으면 됨.
+
 topologicalSort(3, ["ba", "aa", "ab"]);
+// INVALID HYPOTHESIS
+
+topologicalSort(6, [
+    "dictionary",
+    "english",
+    "is",
+    "ordered",
+    "ordinary",
+    "this",
+]);
+// zyxwvusrqpnmlkjhgfdeiotcba
+// d-e-i-o-t
